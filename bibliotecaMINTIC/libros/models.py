e5cd321c56d82a2a6c45947a6fbec5445e370dd9
Richar Chia <rjchiaj@gmail.com>
2022-09-10 22:05:03 -0500

    Reparación error migraciones

--- bibliotecaMINTIC/libros/models.py
+++ bibliotecaMINTIC/libros/models.py
@@ -4,18 +4,21 @@ from django.db import models
 from django.db import models
 
 # Create your models here.
+class cat_editoriales(models.Model):
+    cod_editorial = models.IntegerField(primary_key=True)
+    des_editorial = models.CharField(max_length=255)
+   
+
+
 class cat_autores(models.Model):
     cod_autor = models.IntegerField(primary_key=True)
     des_autor = models.CharField(max_length=255)
 
-class cat_editoriales(models.Model):
-    cod_editorial = models.IntegerField(primary_key=True)
-    des_editorial = models.CharField(max_length=255)
 
 class cat_libros(models.Model):
     cod_libro = models.IntegerField(primary_key=True)
     tit_libro = models.CharField(max_length=255)
-    cod_autor = models.ForeignKey(cat_autores, on_delete=models.CASCADE, related_name="codigoAutor")
+    cod_autor = models.ForeignKey(cat_autores, on_delete=models.CASCADE, related_name="codigoAutor" )
     cod_editorial = models.ForeignKey(cat_editoriales, related_name="codigoEditorial", on_delete=models.CASCADE)
     anio = models.SmallIntegerField()
     tema = models.CharField(max_length=255)
