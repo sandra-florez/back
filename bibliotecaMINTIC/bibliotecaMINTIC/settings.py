5e2b10915da055c2f17820a66264b22877726f95
rinwer <rinsonjawer@gmail.com>
2022-09-20 17:27:34 -0500

    Despliegue de aplicación en heroku.

--- bibliotecaMINTIC/bibliotecaMINTIC/settings.py
+++ bibliotecaMINTIC/bibliotecaMINTIC/settings.py
@@ -140,3 +140,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#solo para heroku
+import django_heroku
+django_heroku.settings(locals())
